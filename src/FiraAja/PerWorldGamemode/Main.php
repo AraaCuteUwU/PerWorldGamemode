0aee81c8b1b94ae28b4845f2410d239ee568df4a
AraaCuteUwU <71029264+AraaCuteUwU@users.noreply.github.com>
2022-09-22 21:34:42 +0800

    Add files via upload

--- src/FiraAja/PerWorldGamemode/Main.php
+++ src/FiraAja/PerWorldGamemode/Main.php
@@ -1,1 +1,18 @@
+<?php
 
+namespace FiraAja\PerWorldGamemode;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase {
+
+    /* @var Config $config */
+    public Config $config;
+
+    public function onEnable(): void {
+        $this->saveResource("config.yml");
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+    }
+}
